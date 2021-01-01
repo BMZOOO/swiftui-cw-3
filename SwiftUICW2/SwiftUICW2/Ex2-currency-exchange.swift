2bd0822e02f07285656c98f0f234a915c72e5b87
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2021-01-01 16:49:16 +0300

    classwork3

--- SwiftUICW2/SwiftUICW2/Ex2-currency-exchange.swift
+++ SwiftUICW2/SwiftUICW2/Ex2-currency-exchange.swift
@@ -7,6 +7,9 @@
 import SwiftUI
 
 struct Exercise2: View {
+    
+    @State var num = ""
+    
     var body: some View {
         
         ZStack {
@@ -26,11 +29,18 @@ struct Exercise2: View {
                         .multilineTextAlignment(.center)
                         .padding()
                     
-                    Text("استبدل هذا ب Text Field")
+                    
+                    
+                    TextField("العمله بالدينار", text: $num )
                         .font(.largeTitle)
                         .multilineTextAlignment(.center)
                         .keyboardType(.decimalPad)
 
+                    let num1 = ( Double(num) ?? 0) * 3.28
+                    let num2 = ( Double(num) ?? 0) * 2.46
+                    let num3 = ( Double(num) ?? 0) * 2.70
+
+                    
                     VStack(spacing: 30){
                         HStack(spacing: 40){
                             Image("us")
@@ -38,7 +48,7 @@ struct Exercise2: View {
                                 .scaledToFit()
                                 .frame(width: 50)
                             
-                            Text("0")
+                            Text("\(num1)")
                         }
                         HStack(spacing: 40){
                             Image("uk")
@@ -46,14 +56,14 @@ struct Exercise2: View {
                                 .scaledToFit()
                                 .frame(width: 50)
                             
-                            Text("0")
+                            Text("\(num2)")
                         }
                         HStack(spacing: 40){
                             Image("eu")
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 50)
-                            Text("0")
+                            Text("\(num3)")
                         }
                     }.padding(.top, 50)
                     Spacer()
